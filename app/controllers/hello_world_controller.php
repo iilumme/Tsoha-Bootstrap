d8d1f85ec929328e30798e19b3ccd00a19b01ecd
iilumme <iilumme@cs.helsinki.fi>
2015-03-13 02:05:56 +0200

    pdf lisays

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,39 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu! :D';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function first_page() {
+        View::make('suunnitelmat/etusivu.html');
+    }
+
+    public static function add_movie() {
+        View::make('movie/leffalisays.html');
+    }
+
+    public static function add_people() {
+        View::make('movie/leffalisaysihmiset.html');
+    }
+
+    public static function register() {
+        View::make('users/rekisteroityminen.html');
+    }
+
+    public static function login() {
+        View::make('users/kirjautuminen.html');
+    }
+
+    public static function search() {
+        View::make('suunnitelmat/haku.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,29 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::index();
-  });
+$routes->get('/', function() {
+    HelloWorldController::first_page();
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+
+$routes->get('/addmovie', function() {
+    HelloWorldController::add_movie();
+});
+
+$routes->get('/addmovie/addpeople', function() {
+    HelloWorldController::add_people();
+});
+
+$routes->get('/register', function() {
+    HelloWorldController::register();
+});
+
+$routes->get('/search', function() {
+    HelloWorldController::search();
+});
